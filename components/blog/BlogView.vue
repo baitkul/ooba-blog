aa35b0e0c51e2920220d0fbc6ee4468895076eb8
amantura <amanturdev@gmail.com>
2021-03-06 10:12:03 +0600

    fix text align, max width

--- components/blog/BlogView.vue
+++ components/blog/BlogView.vue
@@ -1,54 +1,56 @@
 <template>
-  <div v-if="data" class="pb-5 lg:pr-5">
-    <h1 class="text-3xl font-bold text-center">
-      {{ data.title }}
-    </h1>
+  <div>
+    <div v-if="data" class="pb-5 mx-auto max-w lg:pr-5">
+      <h1 class="text-3xl font-bold text-center">
+        {{ data.title }}
+      </h1>
 
-    <div
-      v-for="(blocks, idx) in data.body.blocks"
-      :key="idx"
-      class="mt-7">
-      <p
-        v-if="blocks.type === 'paragraph'"
-        class="text-center paragraph-block"
-        v-html="blocks.data.text">
-      </p>
-      <div v-if="blocks.type === 'image'">
-        <h3 class="text-xl font-bold text-center">{{ blocks.data.caption }}</h3>
-        <div class="flex items-center justify-center mt-2 md:p-5">
-          <img
-            class="object-cover w-full blog-img"
-            :src="blocks.data.file.url"
-            alt="article" />
+      <div
+        v-for="(blocks, idx) in data.body.blocks"
+        :key="idx"
+        class="mt-7">
+        <p
+          v-if="blocks.type === 'paragraph'"
+          class="text-left paragraph-block"
+          v-html="blocks.data.text">
+        </p>
+        <div v-if="blocks.type === 'image'">
+          <h3 class="text-xl font-bold text-center">{{ blocks.data.caption }}</h3>
+          <div class="flex items-center justify-center mt-2 md:p-5">
+            <img
+              class="object-cover w-full blog-img"
+              :src="blocks.data.file.url"
+              alt="article" />
+          </div>
         </div>
-      </div>
-      <div v-if="blocks.type === 'header'">
-        <h1 class="text-2xl font-bold" v-html="blocks.data.text">
-        </h1>
-      </div>
-      <div v-if="blocks.type === 'button'">
-        <div class="flex justify-center w-full py-3">
-          <a
-            class="px-5 py-3 border border-black hover:bg-black hover:text-white"
-            target="_blank"
-            :href="blocks.data.link">{{ blocks.data.input }}</a>
+        <div v-if="blocks.type === 'header'">
+          <h1 class="w-full text-2xl font-bold text-center" v-html="blocks.data.text">
+          </h1>
         </div>
-      </div>
-      <div v-if="blocks.type === 'list'">
-        <ul
-          class="ml-10"
-          style="list-style-type: decimal !important;"
-        >
-          <li
-            v-for="(li, id) in blocks.data.items"
-            :key="id"
+        <div v-if="blocks.type === 'button'">
+          <div class="flex justify-center w-full py-3">
+            <a
+              class="px-5 py-3 border border-black hover:bg-black hover:text-white"
+              target="_blank"
+              :href="blocks.data.link">{{ blocks.data.input }}</a>
+          </div>
+        </div>
+        <div v-if="blocks.type === 'list'">
+          <ul
+            class="ml-10"
+            style="list-style-type: decimal !important;"
           >
-            {{ li }}
-          </li>
-        </ul>
+            <li
+              v-for="(li, id) in blocks.data.items"
+              :key="id"
+            >
+              {{ li }}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
 
+    </div>
     <RecommendSidebar />
 
     <div class="mt-10">
@@ -65,7 +67,6 @@
         </NuxtLink>
       </div>
     </div>
-
   </div>
 </template>
 
